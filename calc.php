e90c3b4e4ba842ef00bbe8176f3bba5f12bcde26
Cheneba <chenebametuge14@gmail.com>
2025-02-20 16:25:59 +0100

    Uploading primary version of my php command line calculator

---
+++ calc.php
@@ -0,0 +1,121 @@
+<?php 
+echo "Welcome to Calculator. We follow BODMAS rules (:\nEnter your calculation to be done:";
+define('ADD', '+');
+define('SUBTRACT', '-');
+define('DIVIDE', '/');
+define('MULTIPLY', '*');
+define('BRACKET_OPEN', '(');
+define('BRACKET_CLOSE', ')');
+define('OF', 'of');
+
+$setter = 1;
+
+function check_operators($arr, $string){
+    global $setter;
+    $setter++;
+    
+    // completely replaces the of with *
+    $string = str_replace(OF, '*', $string);
+    
+
+    switch($setter){
+        case 1:
+            // Checking if either of the operators are present
+            $op_brac = find_operator($arr, BRACKET_OPEN) ? function($arr){
+                return [find_operator($arr, BRACKET_OPEN), find_operator($arr, BRACKET_OPEN)];
+            } : false ;
+
+
+            break;
+        case 2:
+            $op_div = find_operator($arr, DIVIDE);
+            
+            if($op_div){
+                $output = '';
+                if($op_div){
+                    foreach($op_div as $op){
+                        $result = number_format($string[$op-1] / $string[$op+1]);
+                        $output = substr($string, 0, $op-1) . $result . substr($string, $op+2, strlen($string)-1 === $op+2 ? 0 : strlen($string)-1);
+                    } 
+                    $string = $output;
+                    $string_arr = str_split($string, 1);
+                    $string = check_operators($string_arr, $string);
+                }
+            }
+            break;
+        case 3:
+            $op_mul = find_operator($arr, MULTIPLY);
+            
+            if($op_mul){
+                $output = '';
+                if($op_mul){
+                    foreach($op_mul as $op){
+                        $result = $string[$op-1] * $string[$op+1];
+                        $output = substr($string, 0, $op-1) . $result . substr($string, $op+2, strlen($string)-1 === $op+2 ? 0 : strlen($string)-1);
+                    } 
+                    $string = $output;
+                    $string_arr = str_split($string, 1);
+                    $string = check_operators($string_arr, $string);
+                }
+            }
+            break;
+        case 4:
+            $op_add = find_operator($arr, ADD);
+        
+            if($op_add){
+                $output = '';
+                if($op_add){
+                    foreach($op_add as $op){
+                        $result = $string[$op-1] + $string[$op+1];
+                        $output = substr($string, 0, $op-1) . $result . substr($string, $op+2, strlen($string)-1 === $op+2 ? 0 : strlen($string)-1);
+                    } 
+                    $string = $output;
+                    $string_arr = str_split($string, 1);
+                    $string = check_operators($string_arr, $string);
+                }
+            }
+            break;
+
+        case 5:
+            $op_sub = find_operator($arr, SUBTRACT);
+            
+            if($op_sub){
+                $output = '';
+                if($op_sub){
+                    foreach($op_sub as $op){
+                        $result = $string[$op-1] - $string[$op+1];
+                        $output = substr($string, 0, $op-1) . $result . substr($string, $op+2, strlen($string)-1 === $op+2 ? 0 : strlen($string)-1);
+                    } 
+                    $string = $output;
+                    $string_arr = str_split($string, 1);
+                    $string = check_operators($string_arr, $string);
+                }
+            }
+            break;
+        
+    }
+    
+    return $string;
+}
+
+function find_operator($string, $op){
+    $result = [];
+    for($i=0;$i<count($string);$i++){
+        if($string[$i] === $op){
+            $result[] = $i;
+        }
+    } 
+    if($result !== []) return $result;
+    else return null;
+}
+
+$calc = trim(fgets(STDIN)) ?: "5/3*8";
+
+$split = str_split($calc, 1);
+
+
+var_dump(check_operators($split, $calc));
+
+
+
+echo "\n";
