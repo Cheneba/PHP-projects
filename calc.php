cc588df29a446020b6f465e4807ee59bb4be5344
Cheneba <chenebametuge14@gmail.com>
2025-02-21 09:51:44 +0100

    Cleaning the code and commenting

--- calc.php
+++ calc.php
@@ -9,36 +9,32 @@ define('BRACKET_OPEN', '(');
 define('BRACKET_CLOSE', ')');
 define('OF', 'of');
 
-// Variable responsible for moving from one case to another
-$setter = 1;
-
 // Executes the string and returns the result in string form
 function run($arr, $string)
 {
-    global $setter;
-    $setter++;
-
     // completely replaces the "of" with *
     $string = str_replace(OF, '*', $string);
 
+
+    // Calculates and resolves anything that has to do with [+, -, /, *]
     $op_arr = array(DIVIDE, MULTIPLY, ADD, SUBTRACT);
     for ($i = 0; $i < 5; $i++) {
-        $operator = find_operator($arr, $op_arr[$i]);
+        $operator = find_operator($arr, $op_arr[$i]);           // Gets the index of an operator if present
 
         if ($operator) {
             $output = '';
             foreach ($operator as $op) {
-                $result = number_format(execute($op_arr[$i], [$string[$op - 1], $string[$op + 1]]));
+                $result = number_format(execute($op_arr[$i], [$string[$op - 1], $string[$op + 1]]));            // executes the operator and creates output string
                 $output = substr($string, 0, $op - 1) . $result . substr($string, $op + 2, strlen($string) - 1 === $op + 2 ? 0 : strlen($string) - 1);
             }
             $string = $output;
             $arr = str_split($string, 1);
-            $i = -1;
         } else continue;
     }
     return $string;
 }
 
+// Does the explicit execution of 2 numbers according to the operator specified
 function execute($operator, $value)
 {
     if ($operator === DIVIDE) {
@@ -54,6 +50,7 @@ function execute($operator, $value)
     }
 }
 
+// Looks for the operators specified in the input string
 function find_operator($string, $op)
 {
     $result = [];
